15642fea8633492548db5ef93f13836ade65a30f
Manuel Centini <centini.manuel.93@gmail.com>
2023-04-03 15:56:13 +0200

    esercizio 1 settimana 2

---
+++ EserciziSettimana02/day01_03_04_23/assets/js/output.js
@@ -0,0 +1,9 @@
+const nomeAnimale1 = 'criceto', nome_animale_2 = 'giraffa', nomeanimale3 = 'leone', NOMEANIMALE4 = 'pantera'
+
+const stringa1 = document.getElementById('inserto')
+
+stringa1.innerHTML = NOMEANIMALE4
+
+console.log(nomeAnimale1)
+console.log(nome_animale_2)
+console.log(nomeanimale3)
---
+++ EserciziSettimana02/day01_03_04_23/assets/js/write.js
@@ -0,0 +1,4 @@
+// alert('Clicca OK per accedere alla pagina');
+// alert('CLICCA DI NUOVO! Stiamo studiando Javascript!')
+
+document.write('<div id="container_pagina2"><header><h1 id="title_page2">Iniziamo la nostra pratica</h1></header><main><section id="paragrafo"><p>output scritto da javascript</p><p><span id="inserto"></span> scritto da javascript</p><a class="link_alle_pagine" id="link_pagina_index" href="index.html">Torna a pagina 1</a></section></main></div>');
