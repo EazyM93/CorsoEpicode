fc79c02928e617146845f34ed2f07088994375ea
Manuel Centini <centini.manuel.93@gmail.com>
2023-04-17 15:52:07 +0200

    aggiornamento welcome html + javascript

---
+++ BuildWeek1/assets/js/welcomeScript.js
@@ -0,0 +1,8 @@
+// Elementi del DOM - - - - - - - - - - - - - - - - - - - - - - - -
+const difficult = document.getElementById('selectDifficult'); //difficoltà selezionata
+const btnProceed = document.getElementById('proceedButton'); //bottone per procedere
+
+// Prressione bottone per procedere
+btnProceed.addEventListener('click', () => {
+    localStorage.setItem('diff', difficult.value);
+})
