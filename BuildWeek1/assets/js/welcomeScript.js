6ccba6cb6e8f321900a96e88972f20cb25dadb9f
Manuel Centini <centini.manuel.93@gmail.com>
2023-04-17 20:43:55 +0200

    update welcome javascript - blocco e sblocco btn

--- BuildWeek1/assets/js/welcomeScript.js
+++ BuildWeek1/assets/js/welcomeScript.js
@@ -1,8 +1,20 @@
-// Elementi del DOM - - - - - - - - - - - - - - - - - - - - - - - -
-const difficult = document.getElementById('selectDifficult'); //difficoltà selezionata
-const btnProceed = document.getElementById('proceedButton'); //bottone per procedere
+// ---------------------- ELEMENTI DEL DOM ----------------------
 
-// Prressione bottone per procedere
-btnProceed.addEventListener('click', () => {
-    localStorage.setItem('diff', difficult.value);
-});
+const difficult = document.getElementById('selectDifficult'); // difficoltà selezionata
+const btnProceed = document.getElementById('proceedButton'); // bottone per procedere
+const terms = document.getElementById('terms'); // check dei termini per procedere
+
+// ---------------------- SBLOCCO BOTTONE PROCEED ----------------------
+
+btnProceed.disabled = true; // bottone diabilitato
+
+// controlla selezione difficoltà
+difficult.addEventListener('change', () => (terms.checked === true && difficult.value !== '') ? btnProceed.disabled = false : btnProceed.disabled = true);
+
+// controlla checkbox e sblocco / blocco bottone
+terms.addEventListener('change', () => (terms.checked === true && difficult.value !== '') ? btnProceed.disabled = false : btnProceed.disabled = true);
+
+// ---------------------- EVENTO CLICK PROCEED ----------------------
+
+// immagazzina la difficoltà selezionata
+btnProceed.addEventListener('click', () => localStorage.setItem('diff', difficult.value ));
