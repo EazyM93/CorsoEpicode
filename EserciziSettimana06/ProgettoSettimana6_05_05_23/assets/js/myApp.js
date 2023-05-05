6dcdff3c94023d31477205cf13100600ee170c00
Manuel Centini <centini.manuel.93@gmail.com>
2023-05-05 22:12:36 +0200

    up

---
+++ EserciziSettimana06/ProgettoSettimana6_05_05_23/assets/js/myApp.js
@@ -0,0 +1,9 @@
+const courses = [
+    {
+        courseName: 'GraphoL: Introduction to graphQL for beginners',
+        image: 'assets/img/course-graphql.jpg',
+        hours: 2,
+        minutes: 40,
+        
+    }
+]
