7edf1c2eb1779c873f11509f7fbb557c40b0c329
Manuel Centini <centini.manuel.93@gmail.com>
2023-05-12 12:32:28 +0200

    logo cliccabile su home

--- EserciziSettimana07/ProgettoSettimana7_12_05_23/assets/js/pageStructure.js
+++ EserciziSettimana07/ProgettoSettimana7_12_05_23/assets/js/pageStructure.js
@@ -13,7 +13,7 @@ function populateHeader(){
     // popuklate logo & site name
     logo.innerHTML = `<div class="row p-4">
     <div class="col d-flex align-items-center">
-        <img src="assets/img/shopping-cart-304843_640.png" width="50px" class="me-2" alt="">
+    <a href="./homepage.html"><img src="assets/img/shopping-cart-304843_640.png" width="50px" class="me-2" alt=""></a>
         <h2 class="mb-0">CRUDAZON</h2>
     </div>
     </div>`
