a12d3b1087dd55006a753ad831dd385b2a94ed77
Manuel Centini <centini.manuel.93@gmail.com>
2023-05-12 12:29:05 +0200

    creazione dell'header populate

---
+++ EserciziSettimana07/ProgettoSettimana7_12_05_23/assets/js/pageStructure.js
@@ -0,0 +1,31 @@
+// --------------- BASE PAGE BUILD ---------------
+
+// ----- POPULATE HEADER -----
+const header = document.querySelector('header');
+
+function populateHeader(){
+
+    // logo & site name creation
+    const logo = document.createElement('div');
+
+    logo.classList.add('container-fluid');
+
+    // popuklate logo & site name
+    logo.innerHTML = `<div class="row p-4">
+    <div class="col d-flex align-items-center">
+        <img src="assets/img/shopping-cart-304843_640.png" width="50px" class="me-2" alt="">
+        <h2 class="mb-0">CRUDAZON</h2>
+    </div>
+    </div>`
+
+    // html append on header
+    header.appendChild(logo);
+
+}
+
+// ----- ONLOAD POPULATE -----
+window.onload = () => {
+
+    populateHeader();
+
+}
