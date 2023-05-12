38eadfa6e3533474d94c40f39a67223d9439145d
Manuel Centini <centini.manuel.93@gmail.com>
2023-05-12 12:35:33 +0200

    site-name link to home

--- EserciziSettimana07/ProgettoSettimana7_12_05_23/assets/js/pageStructure.js
+++ EserciziSettimana07/ProgettoSettimana7_12_05_23/assets/js/pageStructure.js
@@ -14,7 +14,7 @@ function populateHeader(){
     logo.innerHTML = `<div class="row p-4">
     <div class="col d-flex align-items-center">
     <a href="./homepage.html"><img src="assets/img/shopping-cart-304843_640.png" width="50px" class="me-2" alt=""></a>
-        <h2 class="mb-0">CRUDAZON</h2>
+    <a href="" class="text-decoration-none"><h2 class="mb-0">CRUDAZON</h2></a>
     </div>
     </div>`
 
