d56facee2c7bb5061053727b92eb994dae820722
EazyM93 <centini.manuel.93@gmail.com>
2023-05-23 14:12:37 +0200

    up

--- EserciziSettimana09/day02_23_05_23/assets/js/bankApp.js
+++ EserciziSettimana09/day02_23_05_23/assets/js/bankApp.js
@@ -1,3 +1,4 @@
+"use strict";
 // Clienti
 class FiglioAccount {
     constructor(nome, cognome) {
---
+++ EserciziSettimana09/day02_23_05_23/assets/js/esercizio_vanilla.js
@@ -0,0 +1,76 @@
+"use strict";
+class SonAccount {
+    constructor(nome, cognome) {
+        this.balanceInit = 0;
+        this.nomeCliente = nome;
+        this.cognomeCliente = cognome;
+    }
+    displayCliente() {
+        return `--- INTESTATARIO CONTO CORRENTE : ${this.nomeCliente} ${this.cognomeCliente} ---`;
+    }
+    oneDeposit(versamento) {
+        console.log(`Saldo iniziale: ${this.balanceInit} €`);
+        console.log(`Versamento: ${versamento} €`);
+        this.balanceInit += versamento;
+        return `Saldo attuale: ${this.balanceInit} €`;
+    }
+    oneWithDraw(prelievo) {
+        console.log(`Saldo iniziale: ${this.balanceInit} €`);
+        console.log(`Prelievo: ${prelievo} €`);
+        this.balanceInit -= prelievo;
+        return `Saldo attuale: ${this.balanceInit} €`;
+    }
+    twoDeposit(versamento) {
+        console.log(`Saldo iniziale: ${this.balanceInit} €`);
+        console.log(`Versamento: ${versamento} €`);
+        this.balanceInit += versamento;
+        return `Saldo attuale: ${this.balanceInit} €`;
+    }
+    twoWithDraw(prelievo) {
+        console.log(`Saldo iniziale: ${this.balanceInit} €`);
+        console.log(`Prelievo: ${prelievo} €`);
+        this.balanceInit -= prelievo;
+        return `Saldo attuale: ${this.balanceInit} €`;
+    }
+}
+class MotherAccount extends SonAccount {
+    constructor(nome, cognome) {
+        super(nome, cognome);
+    }
+    oneDeposit(versamento) {
+        console.log(`Saldo iniziale: ${this.balanceInit} €`);
+        console.log(`Versamento: ${versamento} €`);
+        this.balanceInit += versamento;
+        this.balanceInit += (this.balanceInit * 0.1);
+        return `Saldo attuale con interessi (10%): ${this.balanceInit} €`;
+    }
+    twoDeposit(versamento) {
+        console.log(`Saldo iniziale: ${this.balanceInit} €`);
+        console.log(`Versamento: ${versamento} €`);
+        this.balanceInit += versamento;
+        this.balanceInit += (this.balanceInit * 0.1);
+        return `Saldo attuale con interessi (10%): ${this.balanceInit} €`;
+    }
+}
+const manuel = new SonAccount('Manuel', 'Centini');
+console.log(manuel.displayCliente());
+console.log('');
+console.log(manuel.oneDeposit(200));
+console.log('');
+console.log(manuel.oneWithDraw(100));
+console.log('');
+console.log(manuel.twoDeposit(300));
+console.log('');
+console.log(manuel.twoWithDraw(200));
+console.log('');
+const lipa = new MotherAccount('Dua', 'Lipa');
+console.log(lipa.displayCliente());
+console.log('');
+console.log(lipa.oneDeposit(200));
+console.log('');
+console.log(lipa.oneWithDraw(100));
+console.log('');
+console.log(lipa.twoDeposit(300));
+console.log('');
+console.log(lipa.twoWithDraw(200));
+console.log('');
