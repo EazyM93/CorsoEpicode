8e421bb146c39f4c23a900d0e9946ba8f52c53ca
EazyM93 <centini.manuel.93@gmail.com>
2023-05-29 17:28:35 +0200

    up

--- EserciziSettimana10/day01_29_05_23/src/app/app.module.ts
+++ EserciziSettimana10/day01_29_05_23/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ComponentHolaComponent } from './component-hola/component-hola.component';
-import { CompCiaoComponent } from './comp-ciao/comp-ciao.component';
-import { CompCiao2Component } from './comp-ciao2/comp-ciao2.component';
+import { ComponentHolaComponent } from './components/component-hola/component-hola.component';
+import { CompCiaoComponent } from './components/comp-ciao/comp-ciao.component';
+import { CompCiao2Component } from './components/comp-ciao2/comp-ciao2.component';
 
 @NgModule({
   declarations: [
